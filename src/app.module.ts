c4511eeffed2235621bb4751cbc37c2f7a7eacc6
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2023-10-25 09:27:19 -0400

    feat: setup apollo gateway on nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        // ... Apollo server options
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            {
+              name: 'autenticacao',
+              url: 'http://sisgea-module-autenticacao:3471/graphql',
+            },
+          ],
+        }),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
