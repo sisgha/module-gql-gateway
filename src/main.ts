9521474fbcc77102fc624e6b95bcd60c44018c19
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2023-10-25 08:48:29 -0400

    feat: setup development environment

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT ?? null;
+
+  if (port === null) {
+    throw new Error('env.PORT was not provided');
+  }
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
+
 bootstrap();
